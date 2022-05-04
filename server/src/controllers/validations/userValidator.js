af656789330fd94d3042a7e328e82b419be191ba
Gabriel <gabriellopesbusiness@gmail.com>
2022-05-04 13:42:56 -0300

    add user validation

---
+++ server/src/controllers/validations/userValidator.js
@@ -0,0 +1,29 @@
+const { Joi } = require('express-validation')
+
+module.exports = {
+    show: {
+        params: Joi.object({
+            id: Joi.number().integer().required()
+        })
+    },
+    create: {
+        body: Joi.object({
+            name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().min(8).required()
+        })
+    },
+    login: {
+        body: Joi.object({
+            email: Joi.string().email().required(),
+            password: Joi.string().min(8).required()
+        })
+    },
+    update: {
+        body: Joi.object({
+            name: Joi.string().optional(),
+            email: Joi.string().email().optional(),
+            password: Joi.string().min(8).optional()
+        })
+    }
+}
--- server/src/routes/user/index.js
+++ server/src/routes/user/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/userController');
+const userValidator = require('../../controllers/validations/userValidator');
+const { validate } = require('express-validation');
+const auth = require('../../auth');
 
 router.get('/', userController.index);
-router.get('/:id', userController.show);
-router.post('/', userController.create);
-router.post('/login', userController.login);
-router.put('/', auth.required, userController.update);
+router.get('/:id', validate(userValidator.show), userController.show);
+router.post('/', validate(userValidator.create), userController.create);
+router.post('/login', validate(userValidator.login), userController.login);
+router.put('/', auth.required, validate(userValidator.update), userController.update);
 router.delete('/', userController.delete);
 
 module.exports = router;
