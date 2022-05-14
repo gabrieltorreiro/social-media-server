c7e1b7249bdccf48d4ce3875e64da26104a9cf17
Gabrieltorreiro <gabriellopesbusiness@gmail.com>
2022-05-13 23:38:53 -0300

    refact authentication middleware

--- server/src/routes/post/index.js
+++ server/src/routes/post/index.js
@@ -5,21 +5,25 @@ const postValidator = require('../../controllers/validations/postValidator');
 const { validate } = require('express-validation');
 const auth = require('../../auth');
 
+// Required Authentication
+router.use(auth.required);
+
+// Posts
 router.get('/', postController.index);
 router.get('/:id', validate(postValidator.show), postController.show);
-router.post('/', auth.required, validate(postValidator.create), postController.create);
-router.put('/:id', auth.required, validate(postValidator.update), postController.update);
-router.delete('/:id', auth.required, validate(postValidator.delete), postController.delete);
+router.post('/', validate(postValidator.create), postController.create);
+router.put('/:id', validate(postValidator.update), postController.update);
+router.delete('/:id', validate(postValidator.delete), postController.delete);
 
 // Likes
 router.get('/:id/likes', validate(postValidator.getAllLikes), postController.getAllLikes);
-router.get('/:id/like', auth.required, validate(postValidator.getLike), postController.getLike);
-router.put('/:id/like', auth.required, validate(postValidator.setLikeStatus), postController.setLikeStatus);
+router.get('/:id/like', validate(postValidator.getLike), postController.getLike);
+router.put('/:id/like', validate(postValidator.setLikeStatus), postController.setLikeStatus);
 
 // Comments
 router.get('/:id/comments', validate(postValidator.getAllComments), postController.getAllComments);
-router.post('/:id/comments', auth.required, validate(postValidator.createComment), postController.createComment);
-router.put('/:id/comments', auth.required, validate(postValidator.updateComment), postController.updateComment);
-router.delete('/:id/comments', auth.required, validate(postValidator.deleteComment), postController.deleteComment);
+router.post('/:id/comments', validate(postValidator.createComment), postController.createComment);
+router.put('/:id/comments', validate(postValidator.updateComment), postController.updateComment);
+router.delete('/:id/comments', validate(postValidator.deleteComment), postController.deleteComment);
 
 module.exports = router;
