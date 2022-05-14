0e4f487072e8249198ec00a9ee56f609d34a9ac0
Gabrieltorreiro <gabriellopesbusiness@gmail.com>
2022-05-14 00:08:16 -0300

    move BrowserRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Menu from "./components/Menu";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
