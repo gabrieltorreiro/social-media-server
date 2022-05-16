a77b283af951ef153c02a520d0ce0379e93b3da2
Gabrieltorreiro <gabriellopesbusiness@gmail.com>
2022-05-16 04:23:45 -0300

    add IMAGES_PATH

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,4 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 5000,
     DB_URL: process.env.DB_URL || "mysql://root:root@localhost:3306/teste",
     JWT_SECRET: process.env.JWT_SECRET || "secret",
+    IMAGES_PATH: process.env.IMAGES_PATH || __dirname + "/../../uploads",
 }
