5eba7de8daf0e5bcf64b06b45872d740bdc028df
Gabrieltorreiro <gabriellopesbusiness@gmail.com>
2022-05-16 04:24:15 -0300

    create image route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const { ValidationError } = require('express-validation');
+const fs = require('fs');
+const { IMAGES_PATH } = require('../config');
+const path = require('path');
 
 // ROUTERS
 router.use('/user', require('./user'));
 router.use('/post', require('./post'));
+router.get('/image/:imageName', async (req, res, next) => {
+    try {
+        const { imageName } = req.params;
+        if (!fs.existsSync(`./uploads/${imageName}`))
+            throw new Error("Image not found");
+        res.sendFile(path.join(IMAGES_PATH, imageName));
+    } catch (err) {
+        next(err);
+    }
+});
 
 // HANDLE VALIDATION ERRORS
 router.use((err, req, res, next) => {
