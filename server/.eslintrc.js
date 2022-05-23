db1cfa28d92852dfbdb4e2b100ff4dd415f81682
Gabrieltorreiro <gabriellopesbusiness@gmail.com>
2022-05-23 08:32:27 -0300

    add eslint config file

---
+++ server/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        "standard"
+    ],
+    parserOptions: {
+        ecmaVersion: "latest"
+    },
+    rules: {
+        indent: ["error", 4],
+        semi: ["error", "always"],
+        quotes: ["error", "double"]
+    }
+};
