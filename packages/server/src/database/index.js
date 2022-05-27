0e59b0513a95166f45b9ce9bc2b449bbfcf54004
Gabrieltorreiro <gabriellopesbusiness@gmail.com>
2022-05-27 00:53:12 -0300

    handle database connection error

--- packages/server/src/database/index.js
+++ packages/server/src/database/index.js
@@ -1,11 +1,15 @@
+/* eslint-disable no-console */
 const inicializeModels = require("../models");
 const sequelize = require("./sequelize");
 
 async function inicializeDatabase () {
-    await sequelize.authenticate();
-    await inicializeModels();
-    // eslint-disable-next-line no-console
-    console.log("Database is connected");
+    try {
+        await sequelize.authenticate();
+        await inicializeModels();
+        console.log("Database is connected");
+    } catch (err) {
+        console.log("Error connecting to the database: ", err);
+    }
 }
 
 module.exports = inicializeDatabase;
